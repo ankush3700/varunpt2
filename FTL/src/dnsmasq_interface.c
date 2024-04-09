017dd41b73814614413d5ed344be793b74cdb567
ankush3700 <161318427+ankush3700@users.noreply.github.com>
2024-04-09 09:43:53 +0000

    VASDFAVSF

--- FTL/src/dnsmasq_interface.c
+++ FTL/src/dnsmasq_interface.c
@@ -3746,6 +3746,7 @@ bool FTL_model_query(const char* name, union mysockaddr *addr, const unsigned sh
 		if (received_bool){
 			lock_shm();
 			query_blocked(query, domain, client, QUERY_DENYLIST);
+			log_err("%d", (query->status == QUERY_DENYLIST));
 			unlock_shm();
 		}
 		return received_bool;
